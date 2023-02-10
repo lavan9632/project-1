c37cd17dd311b36174c69f1260b1ce9d4f0dda66
lavan <lavandarshu346@gmail.com>
2023-02-10 15:59:38 +0530

    skip staging and commit the file directly

--- pi_b5_proj.py
+++ pi_b5_proj.py
@@ -1,1 +1,8 @@
-print("keerthana is a good girl")
+print("is this the python program ")
+
+a = int(input("enter a number"))
+
+if a % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
